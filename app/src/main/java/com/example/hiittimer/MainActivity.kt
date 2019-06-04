0b0a7761bb4b5453d81fa4832c5ab0cf91e54b2a
Sang Nguyen <snguye39@msudenver.edu>
2019-06-04 14:06:28 -0600

    temp push

--- app/src/main/java/com/example/hiittimer/MainActivity.kt
+++ app/src/main/java/com/example/hiittimer/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.hiittimer
 
+import android.app.Activity
+
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun openNewTimer(view: View) {
+        val launchNewTimer = Intent(this, NewTimer::class.java)
+        startActivityForResult(launchNewTimer, 1)
     }
 }
---
+++ app/src/main/java/com/example/hiittimer/NewTimer.kt
@@ -0,0 +1,5 @@
+package com.example.hiittimer
+
+class NewTimer {
+
+}
